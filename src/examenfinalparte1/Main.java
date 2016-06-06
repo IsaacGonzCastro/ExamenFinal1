91aaca5df011a12709946cfda5a6dcefbba88681
igonzalezcastro <igonzalezcastro@atanasoff07.danielcastelao.org>
2016-06-06 09:29:50 +0200

    Finalizado.

--- src/examenfinalparte1/Main.java
+++ src/examenfinalparte1/Main.java
@@ -13,14 +13,21 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
+        /**
+         * Llamamos al primer metodo.
+         */
         Operaciones obj = new Operaciones(10, 20);
         int n = obj.i;
         System.out.println("Es: " + n);
-
+        /**
+         * llamamos al segundo metodo.
+         */
         Operaciones nm = new Operaciones(10);
         int m = nm.i;
         System.out.println("Es: " + m);
-
+        /**
+         * Llamamos al tercer metodo.
+         */
         Operaciones mn = new Operaciones();
         int nn = mn.i;
         System.out.println("Es: " + nn);
