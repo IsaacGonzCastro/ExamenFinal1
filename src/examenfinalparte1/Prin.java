0a7bfb3eb48d4f75800b91d361d4edc73e3c5002
igonzalezcastro <igonzalezcastro@atanasoff07.danielcastelao.org>
2016-06-06 09:21:50 +0200

    JavaDoc

--- src/examenfinalparte1/Prin.java
+++ src/examenfinalparte1/Prin.java
@@ -1,13 +1,28 @@
 package examenfinalparte1;
+
 import java.io.IOException;
+
+/**
+ *En la clase main,llamamos a los metodos creados en la otra clase.
+ * @author igonzalezcastro
+ */
 public class Prin {
-public static void main(String[] args) throws IOException{
-Principal obj = new Principal(10,20);
-int n = obj.i;
-System.out.println("Es: " + n);
-Principal nm = new Principal(10);
-int m = nm.i;
-System.out.println("Es: " + m);
-Principal mn = new Principal();
-int nn = mn.i;
-System.out.println("Es: " + nn);}}
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        Principal obj = new Principal(10, 20);
+        int n = obj.i;
+        System.out.println("Es: " + n);
+        
+        Principal nm = new Principal(10);
+        int m = nm.i;
+        System.out.println("Es: " + m);
+        
+        Principal mn = new Principal();
+        int nn = mn.i;
+        System.out.println("Es: " + nn);
+    }
+
+}
--- src/examenfinalparte1/Principal.java
+++ src/examenfinalparte1/Principal.java
@@ -1,9 +1,44 @@
 package examenfinalparte1;
+
+/**
+ * En esta clase,es donde se declaran los metodos.
+ * @author igonzalezcastro
+ */
 public class Principal {
+
     int i;
-public Principal(int j, int m) {i=met(j,m);}
+
+    /**
+     *Este metodo le pasamos dos valores y realiza una operacion de suma.
+     * @param j
+     * @param m
+     */
+    public Principal(int j, int m) {
+        i = Operacion(j, m);
+    }
+
+    /**
+     *Este metodo le pasamos dos valores y realiza una operacion de resta.
+     * @param j
+     */
     public Principal(int j) {
-        i=met(j,j);}
-    public Principal() { i=-1;
-    }public int met(int x, int x1) {
-      return x + x1;}}
+        i = Operacion(j, j);
+    }
+
+    /**
+     *Este metodo iguala el valor a -1.
+     */
+    public Principal() {
+        i = -1;
+    }
+
+    /**
+     * En este metodo le pasamos los valores de x,x1 y nos devuelve el resultado de esa operacion.
+     * @param x
+     * @param x1
+     * @return
+     */
+    public int Operacion(int x, int x1) {
+        return x + x1;
+    }
+}
